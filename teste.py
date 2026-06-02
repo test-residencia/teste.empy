c589160b9c7e7971f69b969542ce91c6e93537a4
Kodice <guthergamer@gmail.com>
2026-06-02 13:41:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import getpass
+
 usuario_correto = "admin"
 senha_correta = "1234"
 tentativas_restantes = 3
@@ -6,7 +8,7 @@ print("--- SISTEMA DE LOGIN SIMPLES ---")
 
 while tentativas_restantes > 0:
     usuario_digitado = input("Digite o nome de usuário: ")
-    senha_digitada = input("Digite a senha: ")
+    senha_digitada = getpass.getpass("Digite a senha: ")
 
     if usuario_digitado == usuario_correto and senha_digitada == senha_correta:
         print("\n[SUCESSO] Acesso concedido! Bem-vindo de volta.")
